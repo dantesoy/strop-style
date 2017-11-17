b379f7582b009180fcc5043b24018318bbd83d6b
dantesoy <danteochoaaguilar@gmail.com>
2017-11-17 14:30:36 -0600

    add warning to sidebar

--- source/javascripts/sidebar.js
+++ source/javascripts/sidebar.js
@@ -1,12 +1,14 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "80%";
-    document.body.style.backgroundColor = "rgba(0,0,0,.4)";
+    document.body.style.background = "rgba(0,0,0,.4)";
     document.getElementById("visible-open").classList.remove('is-hidden');
     document.getElementById("is-visible").classList.remove('is-hidden');
     document.getElementById("sidebarContainer").classList.remove('align-flex');
     document.getElementById("sidebarSubcontainer").classList.add('sidebar-subcontainer');
     document.getElementById("sidebarButton").classList.remove('button-dark');
     document.getElementById("sidebarButton").classList.add('button-segment');
+    document.getElementById("segment-view").classList.remove('segment-view-small');
+    document.getElementById("warning").classList.remove('is-hidden');
 }
 
 function closeNav() {
@@ -18,4 +20,6 @@ function closeNav() {
     document.getElementById("sidebarSubcontainer").classList.remove('sidebar-subcontainer');
     document.getElementById("sidebarButton").classList.add('button-dark');
     document.getElementById("sidebarButton").classList.remove('button-segment');
+    document.getElementById("segment-view").classList.add('segment-view-small');
+    document.getElementById("is-visible").classList.add('is-hidden');
 }
