30180234ee375c52a6c422a208454767789325ac
dantesoy <danteochoaaguilar@gmail.com>
2017-11-27 15:52:31 -0600

    footer style

--- source/javascripts/footer.js
+++ source/javascripts/footer.js
@@ -1,6 +1,17 @@
 function openFooter () {
+	document.getElementById("footer-open").classList.remove('is-hidden');
+    document.getElementById("footer-open2").classList.remove('is-hidden');
+    document.getElementById("footer-arrowdown").classList.remove('is-hidden');
+    document.getElementById("footer-arrowup").classList.add('is-hidden');
 }
 
 function closeFooter () {
+	document.getElementById("footer-open").classList.add('is-hidden');
+    document.getElementById("footer-open2").classList.add('is-hidden');
+    document.getElementById("footer-arrowdown").classList.add('is-hidden');
+    document.getElementById("footer-arrowup").classList.remove('is-hidden');
+}
 
+function selectedGraph() {
+	document.getElementById("footer-select").classList.toggle('footer-box-selected');
 }
