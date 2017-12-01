2b8b8e8564315a8cc17dfded6c4cc37ad2f3bce7
dantesoy <danteochoaaguilar@gmail.com>
2017-12-01 11:13:20 -0600

    overlay

--- source/javascripts/footer.js
+++ source/javascripts/footer.js
@@ -3,6 +3,7 @@ function openFooter () {
     document.getElementById("footer-open2").classList.remove('is-hidden');
     document.getElementById("footer-arrowdown").classList.remove('is-hidden');
     document.getElementById("footer-arrowup").classList.add('is-hidden');
+    document.getElementById("overlay").classList.add('footer-overlay');
 }
 
 function closeFooter () {
@@ -10,6 +11,7 @@ function closeFooter () {
     document.getElementById("footer-open2").classList.add('is-hidden');
     document.getElementById("footer-arrowdown").classList.add('is-hidden');
     document.getElementById("footer-arrowup").classList.remove('is-hidden');
+    document.getElementById("overlay").classList.remove('footer-overlay');
 }
 
 function selectedGraph() {
